2a12e912ab967b18fd4678adaddc9e437758aab9
JanneRoe <jetza.jezz@gmail.com>
2024-09-03 18:03:33 -0600

    Added first question

---
+++ LeetCode_questions/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode_questions.Scripts;
+
+while (true)
+{
+
+    Console.Write("Write a word: ");
+    var phrase = Console.ReadLine()!;
+    Console.WriteLine("Please enter k:");
+    var stringnumber = Console.ReadLine()!;
+
+    var number = int.Parse(stringnumber);
+    SumofDigitsofStringAfterConvert sumofDigitsofStringAfterConvert = new SumofDigitsofStringAfterConvert();
+    var sum = sumofDigitsofStringAfterConvert.GetLucky(phrase, number);
+
+
+    Console.WriteLine($"The result is {sum}");
+}
---
+++ LeetCode_questions/Scripts/SumofDigitsofStringAfterConvert.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_questions.Scripts
+{
+    public class SumofDigitsofStringAfterConvert
+    {
+        public int GetLucky(string s, int k)
+        {
+            //string word = s;
+            //int result = 0;
+            //List<int> numbers = new List<int>();
+            //foreach (var letter in word)
+            //{
+            //    numbers.Add(GetLetterNumber(letter));
+            //}
+            //while (k > 0)
+            //{
+            //    result = 0;
+            //    foreach (var number in numbers) 
+            //    {
+            //        result += number;
+            //    }
+            //    word = result.ToString();
+            //    numbers.Clear();
+            //    foreach(var l in word)
+            //    {
+            //        int n;
+            //        if (int.TryParse(l.ToString(), out n))
+            //            numbers.Add(n);
+            //    }
+            //    k--;                
+            //}
+            //return result;
+
+            var h = GetIntLetter(s);
+
+            return h;
+        }
+
+
+        public int GetLetterNumber(char letter)
+        {
+            if (letter == 'a' || letter == 'A' ||
+                letter == 'j' || letter == 'J' || 
+                letter == 's' || letter == 'S') { return 1; }
+            if (letter == 'b' || letter == 'B' || 
+                letter == 'k' || letter == 'K' ||
+                letter == 't' || letter == 'T') { return 2; }
+            if (letter == 'c' || letter == 'C' ||
+                letter == 'l' || letter == 'L' || 
+                letter == 'u' || letter == 'U') { return 3; }
+            if (letter == 'd' || letter == 'D' || 
+                letter == 'm' || letter == 'M' ||
+                letter == 'v' || letter == 'V') { return 4; }
+            if (letter == 'e' || letter == 'E' || 
+                letter == 'n' || letter == 'N' ||
+                letter == 'w' || letter == 'W') { return 5; }
+            if (letter == 'f' || letter == 'F' || 
+                letter == 'o' || letter == 'O' ||
+                letter == 'x' || letter == 'X') { return 6; }
+            if (letter == 'g' || letter == 'G' || 
+                letter == 'p' || letter == 'P' ||
+                letter == 'y' || letter == 'Y') { return 7; }
+            if (letter == 'h' || letter == 'H' ||
+                letter == 'q' || letter == 'Q' ||
+                letter == 'z' || letter == 'Z') { return 8; }
+            if (letter == 'i' || letter == 'I' || 
+                letter == 'r' || letter == 'R') { return 9; }
+            
+            else {  return 0; }
+        }
+
+        public int GetIntLetter(string s)
+        {
+            int sum = 0;
+            foreach (char c in s)
+            {
+                int value = c - 'a' + 1;
+                sum += SumOfDigits(value);
+            }
+            return sum;
+        }
+
+        private int SumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+    
+}
